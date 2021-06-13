4277f9a54303d27ef565581fe44de6c980c49131
orioncohen <orion@lbl.gov>
2021-06-13 12:18:32 -0700

    added wrapped and unwrapped testing files

--- solvation_analysis/tests/datafiles.py
+++ solvation_analysis/tests/datafiles.py
@@ -1,8 +1,9 @@
-__all__ = ["bn_fec.data", "bn_fec_short.dcd"]
+__all__ = ["bn_fec.data", "bn_fec_short_unwrap.dcd"]
 
 from pkg_resources import resource_filename
 
 bn_fec_data = resource_filename(__name__, "data/bn_fec.data")
-bn_fec_dcd = resource_filename(__name__, "data/bn_fec_short.dcd")
+bn_fec_dcd_unwrap = resource_filename(__name__, "data/bn_fec_short_unwrap.dcd")
+bn_fec_dcd_wrap = resource_filename(__name__, "data/bn_fec_short_wrap.dcd")
 
 del resource_filename
