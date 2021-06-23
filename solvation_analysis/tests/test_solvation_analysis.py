44d8dbd9389816ba124ddb3edcd1b71191e20982
orioncohen <orion@lbl.gov>
2021-06-23 16:54:16 -0700

    added more robust testing for solvation selection functions

--- solvation_analysis/tests/test_solvation_analysis.py
+++ solvation_analysis/tests/test_solvation_analysis.py
@@ -106,15 +106,28 @@ def test_get_atom_group(u_real_named):
 
 def test_get_closest_n_mol_grid(u_grid_1):
     u = u_grid_1
-    test_atoms = u.atoms[[0, 5, 10, 16, 42]]
+    test_atoms = u.atoms[[0, 3, 10, 16, 42]]
+    # test that the correct number of residues are being returned
     for test_atom in test_atoms:
-        atoms = get_closest_n_mol(u, test_atom)
+        atoms = get_closest_n_mol(u, test_atom, n_mol=5)
         assert len(atoms) == 6
+    # test that specific correct atoms are being returned
+    expected_shell_ids = {10: [5, 10, 11, 12, 17, 47],
+                         16: [11, 16, 17, 18, 23, 53],
+                         42: [7, 37, 43, 44, 49, 79]}
+    test_atoms = u.atoms[[10, 16, 42]]  # only atoms on side of box
+    for test_atom in test_atoms:
+        expected_ids = expected_shell_ids[test_atom.ix]
+        shell_ids = get_closest_n_mol(u_grid_1, test_atom, n_mol=5).resids
+        np.testing.assert_array_equal(shell_ids, expected_ids)
+
+
 
 
 def test_get_closest_n_mol_real(u_real, atom_groups):
     test_li = atom_groups["li"][0]
     shell_sizes = range(2, 8)
+    # test that the correct number of residues are being returned
     for size in shell_sizes:
         shell = get_closest_n_mol(u_real, test_li, n_mol=size)
         assert len(shell.residues) == size + 1
@@ -123,6 +136,7 @@ def test_get_closest_n_mol_real(u_real, atom_groups):
     default_shell, default_resids, default_radii = get_closest_n_mol(
         u_real, test_li, return_resids=True, return_radii=True
     )
+    # test that the return_radii does not effect behavior
     for rad in radii_range:
         shell, resids, radii = get_closest_n_mol(
             u_real, test_li, radius=rad, return_resids=True, return_radii=True
@@ -133,21 +147,32 @@ def test_get_closest_n_mol_real(u_real, atom_groups):
 
 
 def test_get_radial_shell_grid(u_grid_1):
+    # test that the correct number of atoms are being returned
     u = u_grid_1
     test_atoms = u.atoms[[0, 3, 10, 44]]  # corner, edge, side, center
     distances = [0.95, 1.05, 1.45]
     expected_shell_sizes = {
-        0.95: [1, 1, 1, 1],
-        1.05: [4, 5, 6, 7],
-        1.45: [7, 10, 14, 19],
+        0.95: [1, 1, 1, 1],  # no atoms within 1 A of test atoms because grid size is 1
+        1.05: [4, 5, 6, 7],  # adjacent atoms for corner, edge, side, and center atoms
+        1.45: [7, 10, 14, 19],  # adjacent atoms + diagonally adjacent atoms for ^^
     }
     for distance in distances:
         expected_sizes = expected_shell_sizes[distance]
         shell_sizes = [len(get_radial_shell(u, atom, distance)) for atom in test_atoms]
         np.testing.assert_allclose(expected_sizes, shell_sizes)
+    # test that specific correct atoms are returned
+    expected_shell_ids = {10: [5, 10, 11, 12, 17, 47],
+                         16: [11, 16, 17, 18, 23, 53],
+                         42: [7, 37, 43, 44, 49, 79]}
+    test_atoms = u.atoms[[10, 16, 42]]  # only atoms on side of box
+    for test_atom in test_atoms:
+        expected_ids = expected_shell_ids[test_atom.ix]
+        shell_ids = get_radial_shell(u_grid_1, test_atom, 1.05).resids
+        np.testing.assert_array_equal(shell_ids, expected_ids)
 
 
 def test_get_radial_shell_real(u_real, atom_groups):
+    # test that the correct number of atoms are being returned
     test_li = atom_groups["li"][0]
     radii_range = range(2, 8)
     shell_sizes = [1, 59, 81, 123, 142, 191]
