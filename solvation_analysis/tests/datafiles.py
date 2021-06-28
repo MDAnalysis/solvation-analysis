cd43658a3987b53f85e04168f42ea7bded914f26
orioncohen <orion@lbl.gov>
2021-06-28 15:58:11 -0700

    added rdf data to datafiles.py

--- solvation_analysis/tests/datafiles.py
+++ solvation_analysis/tests/datafiles.py
@@ -1,10 +1,25 @@
-__all__ = ["bn_fec.data", "bn_fec_short_unwrap.dcd"]
-
 from pkg_resources import resource_filename
 
 bn_fec_data = resource_filename(__name__, "data/bn_fec.data")
 bn_fec_dcd_unwrap = resource_filename(__name__, "data/bn_fec_short_unwrap.dcd")
 bn_fec_dcd_wrap = resource_filename(__name__, "data/bn_fec_short_wrap.dcd")
 bn_fec_atom_types = resource_filename(__name__, "data/bn_fec_elements.csv")
+
+rdf_bn_all_atoms_bins = resource_filename(__name__, "data/rdf_bn_all_atoms_bins.csv")
+rdf_bn_all_atoms_data = resource_filename(__name__, "data/rdf_bn_all_atoms_data.csv")
+rdf_bn_N_bins = resource_filename(__name__, "data/rdf_bn_N_bins.csv")
+rdf_bn_N_data = resource_filename(__name__, "data/rdf_bn_N_data.csv")
+rdf_fec_all_atoms_bins = resource_filename(__name__, "data/rdf_fec_all_atoms_bins.csv")
+rdf_fec_all_atoms_data = resource_filename(__name__, "data/rdf_fec_all_atoms_data.csv")
+rdf_fec_F_bins = resource_filename(__name__, "data/rdf_fec_F_bins.csv")
+rdf_fec_F_data = resource_filename(__name__, "data/rdf_fec_F_data.csv")
+rdf_fec_O_bins = resource_filename(__name__, "data/rdf_fec_O_bins.csv")
+rdf_fec_O_data = resource_filename(__name__, "data/rdf_fec_O_data.csv")
+rdf_pf6_all_atoms_bins = resource_filename(__name__, "data/rdf_pf6_all_atoms_bins.csv")
+rdf_pf6_all_atoms_data = resource_filename(__name__, "data/rdf_pf6_all_atoms_data.csv")
+rdf_pf6_F_bins = resource_filename(__name__, "data/rdf_pf6_F_bins.csv")
+rdf_pf6_F_data = resource_filename(__name__, "data/rdf_pf6_F_data.csv")
+rdf_universe_all_atoms_bins = resource_filename(__name__, "data/rdf_universe_all_atoms_bins.csv")
+rdf_universe_all_atoms_data = resource_filename(__name__, "data/rdf_universe_all_atoms_data.csv")
 
 del resource_filename
