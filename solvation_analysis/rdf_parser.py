b6c54b4277de6dfe9557efdf0a904836ad3c0edc
orioncohen <orion@lbl.gov>
2021-06-28 17:16:03 -0700

    basic testing and plotting of rdfs

--- solvation_analysis/rdf_parser.py
+++ solvation_analysis/rdf_parser.py
@@ -1,5 +1,13 @@
 import numpy as np
+import numpy.polynomial.polynomial
+import matplotlib.pyplot as plt
 
 
-def identify_minimum():
+def quick_plot(bins, rdf):
+    plt.plot(bins, rdf, 'bo')
+    plt.show()
+
+
+def identify_minima(rdf, bins, n_minima):
+
     return
--- solvation_analysis/tests/test_rdf_parser.py
+++ solvation_analysis/tests/test_rdf_parser.py
@@ -1,7 +1,22 @@
 import MDAnalysis as mda
 import numpy as np
+import matplotlib.pyplot as plt
 import pytest
+from solvation_analysis.rdf_parser import (
+    quick_plot,
+    identify_minima,
+)
 
 
-def test_identify_minimum():
+@pytest.mark.parametrize(
+    'rdf_tag, minima',
+    [('fec_F', 3),
+     ('fec_O', 3),
+     ('fec_all', 3)]
+)
+def test_identify_minima(rdf_tag, minima, rdf_bins, rdf_data):
+    bins = rdf_bins[rdf_tag]
+    rdf = rdf_data[rdf_tag]
+    quick_plot(bins, rdf)
     return
+
