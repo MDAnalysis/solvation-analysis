14cbec8d40d3678e707ef99f6304ba5f063a6101
orioncohen <orion@lbl.gov>
2021-06-30 16:55:12 -0700

    small type check and added function skeleton

--- solvation_analysis/rdf_parser.py
+++ solvation_analysis/rdf_parser.py
@@ -13,7 +13,9 @@ def interpolate_rdf(bins, rdf, floor=0.05, cutoff=5):
 
 
 def identify_minima(f):
-    assert isinstance(f, scipy.interpolate.fitpack2.InterpolatedUnivariateSpline)
+    assert isinstance(
+        f, scipy.interpolate.fitpack2.InterpolatedUnivariateSpline
+    ), "this function is compatible with the scipy.interpolate package"
     cr_pts = f.derivative().roots()
     cr_vals = f(cr_pts)
     return cr_pts, cr_vals
@@ -28,3 +30,7 @@ def plot_interpolation_fit(bins, rdf, floor=0.05, cutoff=5):
     plt.plot(x, y, "r-")
     plt.plot(pts, vals, "go")
     plt.show()
+
+
+def identify_solvation_cutoff(bins, rdf):
+    return
