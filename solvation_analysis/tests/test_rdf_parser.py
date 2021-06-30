34377ddafe88b015913d56ec9e789e3242ce6219
orioncohen <orion@lbl.gov>
2021-06-30 16:25:20 -0700

    added plotting test to visually confirm results

--- solvation_analysis/tests/test_rdf_parser.py
+++ solvation_analysis/tests/test_rdf_parser.py
@@ -1,22 +1,34 @@
 import MDAnalysis as mda
 import numpy as np
+
 import matplotlib.pyplot as plt
 import pytest
 from solvation_analysis.rdf_parser import (
-    quick_plot,
     identify_minima,
+    interpolate_rdf,
+    plot_interpolation_fit
 )
 
+rdf_parameters = [
+    ("fec_F", []),
+    ("fec_O", [2.9]),
+    ("fec_all", [2.9]),
+    ("bn_all", []),
+    ("bn_N", []),
+    ("pf6_all", []),
+    ("pf6_F", []),
+]
 
-@pytest.mark.parametrize(
-    'rdf_tag, minima',
-    [('fec_F', 3),
-     ('fec_O', 3),
-     ('fec_all', 3)]
-)
-def test_identify_minima(rdf_tag, minima, rdf_bins, rdf_data):
+
+@pytest.mark.parametrize("rdf_tag, minima", rdf_parameters)
+def test_plot_interpolation_fit(rdf_tag, minima, rdf_bins, rdf_data):
+    """This is essentially a visually confirmed regression test to ensure
+    behavior is approximately correct."""
     bins = rdf_bins[rdf_tag]
     rdf = rdf_data[rdf_tag]
-    quick_plot(bins, rdf)
-    return
+    plot_interpolation_fit(bins, rdf)
 
+
+@pytest.mark.parametrize("rdf_tag, minima", rdf_parameters)
+def test_interpolate_rdf(rdf_tag, minima, rdf_bins, rdf_data):
+    return
