2d6e63352abf040ec0f1917bce1cbc015a623bcd
orioncohen <orion@lbl.gov>
2021-06-30 15:15:11 -0700

    interpolation and critical point evaluation for rdfs

--- solvation_analysis/rdf_parser.py
+++ solvation_analysis/rdf_parser.py
@@ -1,13 +1,18 @@
 import numpy as np
 import numpy.polynomial.polynomial
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d, UnivariateSpline
 
 
-def quick_plot(bins, rdf):
-    plt.plot(bins, rdf, 'bo')
-    plt.show()
+def fit_data(bins, rdf):
+    f = UnivariateSpline(bins, rdf, k=4, s=0)
+    x = np.linspace(0, 10, num=100)
+    y = f(x)
+    return x, y
 
 
-def identify_minima(rdf, bins, n_minima):
-
-    return
+def identify_minima(bins, rdf):
+    f = UnivariateSpline(bins, rdf, k=4, s=0)
+    cr_pts = f.derivative().roots()
+    cr_vals = f(cr_pts)
+    return cr_pts, cr_vals
