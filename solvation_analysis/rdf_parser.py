6e5921986e9fe344f1deb052f5ca404124837b20
orioncohen <orion@lbl.gov>
2021-06-30 16:01:50 -0700

    an improved interpolation method

--- solvation_analysis/rdf_parser.py
+++ solvation_analysis/rdf_parser.py
@@ -1,18 +1,24 @@
 import numpy as np
-import numpy.polynomial.polynomial
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d, UnivariateSpline
 
 
-def fit_data(bins, rdf):
-    f = UnivariateSpline(bins, rdf, k=4, s=0)
-    x = np.linspace(0, 10, num=100)
+def fit_data(bins, rdf, floor=0.05, cutoff=5):
+    start = np.argmax(rdf > floor)  # will return first index > rdf
+    end = np.argmax(bins > cutoff)  # will return first index > cutoff
+    bounds = (bins[start], bins[end - 1])
+    f = UnivariateSpline(bins[start:end], rdf[start:end], k=4, s=0)
+    return f, bounds
+
+
+def array_of_fit(bins, rdf):
+    f, bounds = fit_data(bins, rdf)
+    x = np.linspace(bounds[0], bounds[1], num=100)
     y = f(x)
     return x, y
 
 
 def identify_minima(bins, rdf):
-    f = UnivariateSpline(bins, rdf, k=4, s=0)
+    f, bounds= fit_data(bins, rdf)
     cr_pts = f.derivative().roots()
     cr_vals = f(cr_pts)
     return cr_pts, cr_vals
