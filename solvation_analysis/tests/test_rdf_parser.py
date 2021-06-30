a16b8b06e358f1f6bf751fa87453fb509499ff35
orioncohen <orion@lbl.gov>
2021-06-30 16:55:31 -0700

    created testing skeleton with faux parameterization

--- solvation_analysis/tests/test_rdf_parser.py
+++ solvation_analysis/tests/test_rdf_parser.py
@@ -6,10 +6,11 @@ import pytest
 from solvation_analysis.rdf_parser import (
     identify_minima,
     interpolate_rdf,
-    plot_interpolation_fit
+    plot_interpolation_fit,
 )
 
-rdf_parameters = [
+
+rdf_minima = [
     ("fec_F", []),
     ("fec_O", [2.9]),
     ("fec_all", [2.9]),
@@ -20,15 +21,64 @@ rdf_parameters = [
 ]
 
 
-@pytest.mark.parametrize("rdf_tag, minima", rdf_parameters)
-def test_plot_interpolation_fit(rdf_tag, minima, rdf_bins, rdf_data):
+@pytest.mark.parametrize(
+    "rdf_tag",
+    ["fec_F", "fec_O", "fec_all", "bn_all", "bn_N", "pf6_all", "pf6_F"],
+)
+def test_plot_interpolation_fit(rdf_tag, rdf_bins_and_data):
     """This is essentially a visually confirmed regression test to ensure
     behavior is approximately correct."""
-    bins = rdf_bins[rdf_tag]
-    rdf = rdf_data[rdf_tag]
+    bins, rdf = rdf_bins_and_data[rdf_tag]
     plot_interpolation_fit(bins, rdf)
+    # add code to save teh plot so that it can be easily viewed later
 
 
-@pytest.mark.parametrize("rdf_tag, minima", rdf_parameters)
-def test_interpolate_rdf(rdf_tag, minima, rdf_bins, rdf_data):
+@pytest.mark.parametrize(
+    "rdf_tag, bounds",
+    [
+        ("fec_F", (0, 3)),
+        ("fec_O", (0, 3)),
+        ("fec_all", (0, 3)),
+        ("bn_all", (0, 3)),
+        ("bn_N", (0, 3)),
+        ("pf6_all", (0, 3)),
+        ("pf6_F", (0, 3)),
+    ],  # the above values are not real
+)
+def test_interpolate_rdf(rdf_tag, bounds, rdf_bins_and_data):
+    bins, rdf = rdf_bins_and_data[rdf_tag]
+    return
+
+
+@pytest.mark.parametrize(
+    "rdf_tag, minima",
+    [
+        ("fec_F", [1, 2, 3]),
+        ("fec_O", [1, 2, 3]),
+        ("fec_all", [1, 2, 3]),
+        ("bn_all", [1, 2, 3]),
+        ("bn_N", [1, 2, 3]),
+        ("pf6_all", [1, 2, 3]),
+        ("pf6_F", [1, 2, 3]),
+    ],  # the above values are not real
+)
+def test_identify_minima(rdf_tag, minima, rdf_bins_and_data):
+    bins, rdf = rdf_bins_and_data[rdf_tag]
+    return
+
+
+@pytest.mark.parametrize(
+    "rdf_tag, cutoff",
+    [
+        ("fec_F", 2.8),
+        ("fec_O", 2.9),
+        ("fec_all", 2.9),
+        ("bn_all", 3),
+        ("bn_N", 3),
+        ("pf6_all", 3),
+        ("pf6_F", 3),
+    ],  # the above values are not real
+)
+def test_identify_solvation_cutoff(rdf_tag, cutoff, rdf_bins_and_data):
+    bins, rdf = rdf_bins_and_data[rdf_tag]
     return
