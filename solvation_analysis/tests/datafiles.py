2cea4654d1872a1d18b021462ceff1827de91bb8
orioncohen <orion@lbl.gov>
2021-07-02 11:49:08 -0700

    reworked data imports to cut down on far too many explicit path names

--- solvation_analysis/tests/datafiles.py
+++ solvation_analysis/tests/datafiles.py
@@ -1,25 +1,39 @@
 from pkg_resources import resource_filename
+import pathlib
+from pathlib import Path
 
 bn_fec_data = resource_filename(__name__, "data/bn_fec.data")
 bn_fec_dcd_unwrap = resource_filename(__name__, "data/bn_fec_short_unwrap.dcd")
 bn_fec_dcd_wrap = resource_filename(__name__, "data/bn_fec_short_wrap.dcd")
 bn_fec_atom_types = resource_filename(__name__, "data/bn_fec_elements.csv")
 
-rdf_bn_all_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_bn_all_bins.csv")
-rdf_bn_all_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_bn_all_data.csv")
-rdf_bn_N_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_bn_N_bins.csv")
-rdf_bn_N_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_bn_N_data.csv")
-rdf_fec_all_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_fec_all_bins.csv")
-rdf_fec_all_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_fec_all_data.csv")
-rdf_fec_F_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_fec_F_bins.csv")
-rdf_fec_F_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_fec_F_data.csv")
-rdf_fec_O_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_fec_O_bins.csv")
-rdf_fec_O_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_fec_O_data.csv")
-rdf_pf6_all_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_pf6_all_bins.csv")
-rdf_pf6_all_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_pf6_all_data.csv")
-rdf_pf6_F_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_pf6_F_bins.csv")
-rdf_pf6_F_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_pf6_F_data.csv")
-rdf_universe_all_bins = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_universe_all_bins.csv")
-rdf_universe_all_data = resource_filename(__name__, "data/bn_fec_rdf_vs_li/rdf_universe_all_data.csv")
+data_dir = Path('data')
+easy_rdf_dir = data_dir / 'rdf_vs_li_easy'
+hard_rdf_dir = data_dir / 'rdf_vs_li_hard'
+fail_rdf_dir = data_dir / 'rdf_non_solvated'
+
+easy_rdf_bins = {}
+for rdf_path in easy_rdf_dir.glob('*bins.csv'):
+    tag_list = rdf_path.stem.split('_')
+    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
+    easy_rdf_bins[rdf_tag] = resource_filename(__name__, str(rdf_path))
+
+easy_rdf_data = {}
+for rdf_path in easy_rdf_dir.glob('*data.csv'):
+    tag_list = rdf_path.stem.split('_')
+    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
+    easy_rdf_data[rdf_tag] = resource_filename(__name__, str(rdf_path))
+
+hard_rdf_bins = {}
+for rdf_path in easy_rdf_dir.glob('*bins.csv'):
+    tag_list = rdf_path.stem.split('_')
+    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
+    hard_rdf_bins[rdf_tag] = resource_filename(__name__, str(rdf_path))
+
+hard_rdf_data = {}
+for rdf_path in easy_rdf_dir.glob('*data.csv'):
+    tag_list = rdf_path.stem.split('_')
+    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
+    hard_rdf_data[rdf_tag] = resource_filename(__name__, str(rdf_path))
 
 del resource_filename
