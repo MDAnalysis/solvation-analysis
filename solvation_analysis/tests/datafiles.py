2f3520fc9df374efcb3364b72e097ba241f8fcbd
orioncohen <orion@lbl.gov>
2021-07-07 13:55:19 -0700

    refactored datafiles.py, hoping to pass CI

--- solvation_analysis/tests/datafiles.py
+++ solvation_analysis/tests/datafiles.py
@@ -7,45 +7,46 @@ bn_fec_dcd_unwrap = resource_filename(__name__, "data/bn_fec_short_unwrap.dcd")
 bn_fec_dcd_wrap = resource_filename(__name__, "data/bn_fec_short_wrap.dcd")
 bn_fec_atom_types = resource_filename(__name__, "data/bn_fec_elements.csv")
 
-data_dir = Path('data')
-easy_rdf_dir = data_dir / 'rdf_vs_li_easy'
-hard_rdf_dir = data_dir / 'rdf_vs_li_hard'
-fail_rdf_dir = data_dir / 'rdf_non_solvated'
+data_dir = Path("data")
+easy_rdf_dir = data_dir / "rdf_vs_li_easy"
+hard_rdf_dir = data_dir / "rdf_vs_li_hard"
+fail_rdf_dir = data_dir / "rdf_non_solvated"
 
-easy_rdf_bins = {}
-for rdf_path in easy_rdf_dir.glob('*bins.npz'):
-    tag_list = rdf_path.stem.split('_')
-    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
-    easy_rdf_bins[rdf_tag] = resource_filename(__name__, str(rdf_path))
 
-easy_rdf_data = {}
-for rdf_path in easy_rdf_dir.glob('*data.npz'):
-    tag_list = rdf_path.stem.split('_')
+def generate_tag(filename):
+    tag_list = filename.stem.split("_")
     rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
-    easy_rdf_data[rdf_tag] = resource_filename(__name__, str(rdf_path))
+    return rdf_tag
 
-hard_rdf_bins = {}
-for rdf_path in hard_rdf_dir.glob('*bins.npz'):
-    tag_list = rdf_path.stem.split('_')
-    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
-    hard_rdf_bins[rdf_tag] = resource_filename(__name__, str(rdf_path))
 
-hard_rdf_data = {}
-for rdf_path in hard_rdf_dir.glob('*data.npz'):
-    tag_list = rdf_path.stem.split('_')
-    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
-    hard_rdf_data[rdf_tag] = resource_filename(__name__, str(rdf_path))
+easy_rdf_bins = {
+    generate_tag(rdf_path): resource_filename(__name__, str(rdf_path))
+    for rdf_path in easy_rdf_dir.glob("*bins.npz")
+}
 
-non_solv_rdf_bins = {}
-for rdf_path in fail_rdf_dir.glob('*data.npz'):
-    tag_list = rdf_path.stem.split('_')
-    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
-    non_solv_rdf_bins[rdf_tag] = resource_filename(__name__, str(rdf_path))
+easy_rdf_data = {
+    generate_tag(rdf_path): resource_filename(__name__, str(rdf_path))
+    for rdf_path in easy_rdf_dir.glob("*data.npz")
+}
 
-non_solv_rdf_data = {}
-for rdf_path in fail_rdf_dir.glob('*data.npz'):
-    tag_list = rdf_path.stem.split('_')
-    rdf_tag = f"{tag_list[1]}_{tag_list[2]}"
-    non_solv_rdf_data[rdf_tag] = resource_filename(__name__, str(rdf_path))
+hard_rdf_bins = {
+    generate_tag(rdf_path): resource_filename(__name__, str(rdf_path))
+    for rdf_path in hard_rdf_dir.glob("*bins.npz")
+}
+
+hard_rdf_data = {
+    generate_tag(rdf_path): resource_filename(__name__, str(rdf_path))
+    for rdf_path in hard_rdf_dir.glob("*data.npz")
+}
+
+non_solv_rdf_bins = {
+    generate_tag(rdf_path): resource_filename(__name__, str(rdf_path))
+    for rdf_path in fail_rdf_dir.glob("*bins.npz")
+}
+
+non_solv_rdf_data = {
+    generate_tag(rdf_path): resource_filename(__name__, str(rdf_path))
+    for rdf_path in fail_rdf_dir.glob("*data.npz")
+}
 
 del resource_filename
