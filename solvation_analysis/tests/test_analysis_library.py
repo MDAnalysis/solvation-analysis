b06d28b2acd9a1c1bafd66da32bea1d2c8633260
orioncohen <orion@lbl.gov>
2021-07-19 10:41:03 -0800

    added scaffolding for analysis_library functions

---
+++ solvation_analysis/tests/test_analysis_library.py
@@ -0,0 +1,17 @@
+from solvation_analysis.analysis_library import (
+    _calculate_ion_speciation,
+    _calculate_coordination_numbers,
+    _calculate_ion_pairing,
+)
+
+
+def test_calculate_ion_speciation():
+    return
+
+
+def test_calculate_coordination_numbers():
+    return
+
+
+def test_calculate_ion_pairing():
+    return
