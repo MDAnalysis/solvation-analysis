561983d6e7817b06e0e8d93189b6c16e46e1a7d8
orioncohen <orion@lbl.gov>
2021-07-19 12:10:18 -0800

    basic testing for Solute class

--- solvation_analysis/tests/test_analysis.py
+++ solvation_analysis/tests/test_analysis.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pytest
 
+from copy import deepcopy
+
 from solvation_analysis.analysis import Solute
 
 
@@ -13,6 +15,12 @@ def default_solute(atom_groups):
     return Solute(li, {'pf6': pf6, 'bn': bn, 'fec': fec})
 
 
+@pytest.fixture
+def prepared_solute(default_solute):
+    default_solute.run_prepare()
+    return default_solute  # TODO: will this work?
+
+
 def test_plot_solvation_distance(rdf_bins_and_data_easy):
     bins, data = rdf_bins_and_data_easy['pf6_all']
     fig, ax = Solute._plot_solvation_radius(bins, data, 2)
@@ -22,3 +30,9 @@ def test_plot_solvation_distance(rdf_bins_and_data_easy):
 def test_run_prepare(default_solute):
     default_solute.run_prepare()
     return
+
+
+def test_run(prepared_solute):
+    prepared_solute.run()
+
+
