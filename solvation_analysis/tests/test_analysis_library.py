43c67c511c52123d4c876a35067e849b0b6e72b9
orioncohen <orion@lbl.gov>
2021-07-20 14:35:26 -0800

    small changes to testing

--- solvation_analysis/tests/test_analysis_library.py
+++ solvation_analysis/tests/test_analysis_library.py
@@ -7,11 +7,12 @@ from solvation_analysis.analysis_library import (
 )
 
 
-
-
-
 def test_ion_speciation(solvation_results):
     speciation = _IonSpeciation(solvation_results)
+
+
+def test_coordination_single(solvation_results):
+    result = _CoordinationNumbers._single_frame(solvation_results[0])
 
 
 def test_coordination_numbers(solvation_results):
