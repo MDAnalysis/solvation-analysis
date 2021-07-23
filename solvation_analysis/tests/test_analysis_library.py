af5f0d5b38f6f4d465c42f782cba4fcb5edbe11d
orioncohen <orion@lbl.gov>
2021-07-22 17:11:09 -0800

    testing for the analysis library functions

--- solvation_analysis/tests/test_analysis_library.py
+++ solvation_analysis/tests/test_analysis_library.py
@@ -1,23 +1,49 @@
+import numpy as np
 import pytest
 
 from solvation_analysis.analysis_library import (
     _IonSpeciation,
-    _CoordinationNumbers,
-    _IonPairing,
+    _CoordinationNumber,
+    _Pairing,
+    _SolutionAnalysis,
 )
 
+@pytest.mark.parametrize(
+    "name, percent",
+    [
+        ((4, 0, 0), 0.295),
+        ((5, 0, 0), 0.357),
+        ((3, 3, 0), 0.004),
+        ((3, 0, 1), 0.016),
+    ],
+)
+def test_ion_speciation(name, percent, solvation_results):
+    speciation = _IonSpeciation(solvation_results).average_speciation
+    np.testing.assert_allclose(percent, speciation[name], atol=0.05)
 
-def test_ion_speciation(solvation_results):
-    speciation = _IonSpeciation(solvation_results)
+
+@pytest.mark.parametrize(
+    "name, cn",
+    [
+        ("fec", 0.25),
+        ("bn", 4.33),
+        ("pf6", 0.15),
+    ],
+)
+def test_coordination_numbers(name, cn, solvation_results):
+    coord_dict = _CoordinationNumber(solvation_results).average_dict
+    np.testing.assert_allclose(cn, coord_dict[name], atol=0.05)
 
 
-def test_coordination_single(solvation_results):
-    result = _CoordinationNumbers._single_frame(solvation_results[0])
-
-
-def test_coordination_numbers(solvation_results):
-    coordination = _CoordinationNumbers(solvation_results)
-
-
-def test_ion_pairing(solvation_results):
-    pairing = _IonPairing(solvation_results)
+@pytest.mark.parametrize(
+    "name, percent",
+    [
+        ("fec", 0.21),
+        ("bn", 1.0),
+        ("pf6", 0.14),
+    ],
+)
+def test_pairing(name, percent, solvation_results):
+    pairing_dict = _Pairing(solvation_results).percentage_dict
+    np.testing.assert_allclose([percent], pairing_dict[name], atol=0.05)
+    return
