0b40681026833857a39e7a5b496c64aee6769ccf
orioncohen <orion@lbl.gov>
2021-08-09 17:27:30 -0700

    improved testing for Speciation

--- solvation_analysis/tests/test_analysis_library.py
+++ solvation_analysis/tests/test_analysis_library.py
@@ -15,13 +15,28 @@ from solvation_analysis.analysis_library import (
         ({'bn': 5, 'fec': 0, 'pf6': 0}, 0.357),
         ({'bn': 3, 'fec': 3, 'pf6': 0}, 0.004),
         ({'bn': 3, 'fec': 0, 'pf6': 1}, 0.016),
+        ({'bn': 4}, 0.531),
     ],
 )
-def test_ion_speciation(cluster, percent, solvation_data):
+def test_speciation_cluster_percent(cluster, percent, solvation_data):
     speciation = _Speciation(solvation_data, 10, 49)
-    speciation.find_clusters({'bn': 5})
     percentage = speciation.cluster_percent(cluster)
     np.testing.assert_allclose(percent, percentage, atol=0.05)
+
+
+@pytest.mark.parametrize(
+    "cluster, n_clusters",
+    [
+        ({'bn': 5, 'fec': 0, 'pf6': 0}, 175),
+        ({'bn': 3, 'fec': 3, 'pf6': 0}, 2),
+        ({'bn': 3, 'fec': 0, 'pf6': 1}, 8),
+        ({'bn': 4}, 260),
+    ],
+)
+def test_speciation_find_clusters(cluster, n_clusters, solvation_data):
+    speciation = _Speciation(solvation_data, 10, 49)
+    df = speciation.find_clusters(cluster)
+    assert len(df) == n_clusters
 
 
 @pytest.mark.parametrize(
