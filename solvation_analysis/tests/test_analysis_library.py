2cbdd5651cb6916165e05f468fb8ec137707d5bd
orioncohen <orion@lbl.gov>
2021-09-22 21:20:41 -0700

    testing for the calculate coordinating atoms functionality

--- solvation_analysis/tests/test_analysis_library.py
+++ solvation_analysis/tests/test_analysis_library.py
@@ -66,10 +66,25 @@ def test_plot_correlation(solvation_data):
         ("pf6", 0.15),
     ],
 )
-def test_coordination(name, cn, solvation_data):
-    coordination = Coordination(solvation_data, 10, 49)
+def test_coordination(name, cn, solvation_data, run_solution):
+    atoms = run_solution.u.atoms
+    coordination = Coordination(solvation_data, 10, 49, atoms)
     np.testing.assert_allclose(cn, coordination.cn_dict[name], atol=0.05)
     assert len(coordination.cn_by_frame) == 3
+
+@pytest.mark.parametrize(
+    "name, atom_type, percent",
+    [
+        ("fec", 19, 0.008),
+        ("bn", 5, 0.9976),
+        ("pf6", 21, 1.000),
+    ],
+)
+def test_coordinating_atoms(name, atom_type, percent, solvation_data, run_solution):
+    atoms = run_solution.u.atoms
+    coordination = Coordination(solvation_data, 10, 49, atoms)
+    calculated_percent = coordination.coordinating_atoms.loc[(name, atom_type)]
+    np.testing.assert_allclose(percent, calculated_percent, atol=0.05)
 
 
 @pytest.mark.parametrize(
