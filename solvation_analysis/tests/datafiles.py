03d78a1fa225fdefa828fdfd669a3c09aa6a7201
orioncohen <orion@lbl.gov>
2022-01-25 17:46:13 -0800

    modify datafiles to support new test files

--- solvation_analysis/tests/datafiles.py
+++ solvation_analysis/tests/datafiles.py
@@ -6,6 +6,8 @@ bn_fec_data = resource_filename(__name__, "data/bn_fec.data")
 bn_fec_dcd_unwrap = resource_filename(__name__, "data/bn_fec_short_unwrap.dcd")
 bn_fec_dcd_wrap = resource_filename(__name__, "data/bn_fec_short_wrap.dcd")
 bn_fec_atom_types = resource_filename(__name__, "data/bn_fec_elements.csv")
+ec_emc_dcd = resource_filename(__name__, "data/ec_emc.dcd")
+ec_emc_pdb = resource_filename(__name__, "data/ec_emc.pdb")
 
 test_dir = Path(__file__).parent
 data_dir = test_dir / "data"
