436c10dfea075fad281ccba802ed927572a1402a
orioncohen <orion@lbl.gov>
2022-05-18 14:10:57 -0700

    basic skeleton for visualization module

---
+++ solvation_analysis/visualization.py
@@ -0,0 +1,33 @@
+
+
+
+
+# low level
+
+
+
+# high level“
+
+def plot_pairing():
+    return
+
+
+def plot_free_solvent():
+
+
+def plot_speciation():
+    return
+
+
+def plot_coordination_number():
+    return
+
+
+def plot_co_occurrence():
+    return
+
+# comparison level
+
+
+def compare_speciation():
+    return
