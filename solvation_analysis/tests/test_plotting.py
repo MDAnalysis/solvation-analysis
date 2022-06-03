45191196156df0ff0da09d5141a129ac099f48f9
orioncohen <orion@lbl.gov>
2022-06-03 14:47:11 -0700

    added test plotting fix

--- solvation_analysis/tests/test_plotting.py
+++ solvation_analysis/tests/test_plotting.py
@@ -1,9 +1,19 @@
 import numpy as np
-from string import ascii_lowercase
+import pytest
 
-from solvation_analysis.plotting import square_area
+from solvation_analysis.plotting import (
+    plot_network_size_histogram,
+    plot_shell_size_histogram,
+)
 
-def test_square_area():
-    data = np.array([50, 30, 10, 4, 4, 4, 3, 1, 1, 1, 1, 1])
-    labels = np.array([char for char in ascii_lowercase[0: len(data)]], dtype='<U100')
-    square_area(data, labels)
+
+def test_plot_network_size_histogram(networking):
+    fig = plot_network_size_histogram(networking)
+    fig.show()
+    assert True
+
+
+def test_plot_shell_size_histogram(run_solution):
+    fig = plot_shell_size_histogram(run_solution)
+    fig.show()
+    assert True
