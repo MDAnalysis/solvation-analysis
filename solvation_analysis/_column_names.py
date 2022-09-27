940ef2c1eaa4b405a72312a1391bafd892d9920d
orioncohen <orion@lbl.gov>
2022-09-27 10:42:12 -0700

    add a column names file

---
+++ solvation_analysis/_column_names.py
@@ -0,0 +1,11 @@
+
+FRAME = "frame"
+SOLVATED_ATOM = "solvated_atom"
+ATOM_IX = "atom_ix"
+DISTANCE = "dist"
+RESNAME = "resname"
+RES_IX = "res_ix"
+
+
+
+# [FRAME, "solvated_atom", "atom_ix", "dist", "res_name", "res_ix"]
